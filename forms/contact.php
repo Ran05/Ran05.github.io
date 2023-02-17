5265f6ca6e765c0f26fd66575a25fd3882245919
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2023-02-17 13:28:30 +0800

    setting up smtp

--- forms/contact.php
+++ forms/contact.php
@@ -19,12 +19,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  // $contact->smtp = array(
-  //   'host' => 'example.com',
-  //   'username' => 'example',
-  //   'password' => 'pass',
-  //   'port' => '587'
-  // );
+  $contact->smtp = array(
+    'host' => 'smtp-relay.sendinblue.com',
+    'username' => 'ferarizarandolfh@gmail.com',
+    'password' => 'rfK70xqvB2zDjQbs',
+    'port' => '587'
+  );
   
 
   $contact->add_message( $_POST['name'], 'From');
